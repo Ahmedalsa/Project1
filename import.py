8c86926fb166d6e4ed751f8560842ba1611fc88b
Ahmedalsa <42217218+Ahmedalsa@users.noreply.github.com>
2019-08-18 09:17:23 +0300

    changed the query string.

--- import.py
+++ import.py
@@ -3,34 +3,45 @@
 import csv
 #load psycopg2 libraries
 import psycopg2
+import sys
+import logging
+
 
 #set up connection to the database
 
-conn = psycopg2.connect("dbname='d7e9ku4tq19i8e' user='jrmqlumszscete' host='ec2-107-20-198-176.compute-1.amazonaws.com' password='7a7a8b6d39ca502a1f037d56b887670a944ef7eeac3773d84447a0fa4f1f93b1'")
+conn = psycopg2.connect(
+           host='ec2-107-20-198-176.compute-1.amazonaws.com',
+           database='d7e9ku4tq19i8e',
+           user='jrmqlumszscete',
+           password='7a7a8b6d39ca502a1f037d56b887670a944ef7eeac3773d84447a0fa4f1f93b1')
 
 
 #use cursor object to execute commands in psycopg2
 
 cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE data
-           (ID INT PRIMARY KEY        NOT NULL,
-           isbn CHAR(50) ,
-           title CHAR(50) ,
-           author CHAR(50) ,
-           year INT
+reader = csv.reader(open('books.csv', 'r'))
+
+
+cursor.execute('''CREATE TABLE books
+           (id SERIAL PRIMARY KEY,
+           isbn CHAR(100) NOT NULL,
+           title CHAR(100) NOT NULL,
+           author CHAR(100) NOT NULL,
+           year INT NOT NULL
            );''')
 
-reader = csv.readr(open('books.csv', 'r'))
 
-for i, row in enumerate(reader):
-    print(i, row)
-    if i == 0: continue
-
-    cur.execute('''
-        INSERT INTO "data" (
-            "isbn", "title", "author", "year"
-        ) values %s''', [tuple(row)]
-    )
+for line in file:
+    my_list = line.split(',')
+    string = ''
+    for value in my_list:
+        string = string + "'" + value + "',"
+        sql_string = string[:-1]
+    last_query = "INSERT INTO books (isbn, title, author, year) VALUES"+"("+sql_string+");"
+    cursor.execute(last_query)
     conn.commit()
-    cur.close()
+
+        #close the connection
+cursor.close()
+conn.close()

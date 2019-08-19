297e1516426de77b693e5209c10a007aa85a345d
Ahmedalsa <42217218+Ahmedalsa@users.noreply.github.com>
2019-08-19 20:12:53 +0300

    database books has been imported.

--- import.py
+++ import.py
@@ -1,47 +1,36 @@
 
 #load  csv libraries
 import csv
+import os
 #load psycopg2 libraries
 import psycopg2
-import sys
-import logging
 
+#load sqlalchemy libraries
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 #set up connection to the database
 
-conn = psycopg2.connect(
-           host='ec2-107-20-198-176.compute-1.amazonaws.com',
-           database='d7e9ku4tq19i8e',
-           user='jrmqlumszscete',
-           password='7a7a8b6d39ca502a1f037d56b887670a944ef7eeac3773d84447a0fa4f1f93b1')
+connection = create_engine('postgres://jrmqlumszscete:7a7a8b6d39ca502a1f037d56b887670a944ef7eeac3773d84447a0fa4f1f93b1@ec2-107-20-198-176.compute-1.amazonaws.com:5432/d7e9ku4tq19i8e')
+sql = scoped_session(sessionmaker(bind=connection))
 
+def main():
+    sql.execute("""CREATE TABLE books (
+                   id       SERIAL PRIMARY KEY,
+                   isbn VARCHAR(50)   NOT NULL,
+                   title VARCHAR(50)  NOT NULL,
+                   author VARCHAR(50) NOT NULL,
+                   year INT           NOT NULL
+                  );""")
+    f = open("books.csv")
+    reader = csv.reader(f)
+    next(reader, None)
+    for isbn, title, author, year in reader:
+        sql.execute(
+            "INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+            {"isbn": isbn, "title": title, "author": author, "year": year}
+        )
+    sql.commit()
 
-#use cursor object to execute commands in psycopg2
-
-cursor = conn.cursor()
-
-reader = csv.reader(open('books.csv', 'r'))
-
-
-cursor.execute('''CREATE TABLE books
-           (id SERIAL PRIMARY KEY,
-           isbn CHAR(100) NOT NULL,
-           title CHAR(100) NOT NULL,
-           author CHAR(100) NOT NULL,
-           year INT NOT NULL
-           );''')
-
-
-for line in file:
-    my_list = line.split(',')
-    string = ''
-    for value in my_list:
-        string = string + "'" + value + "',"
-        sql_string = string[:-1]
-    last_query = "INSERT INTO books (isbn, title, author, year) VALUES"+"("+sql_string+");"
-    cursor.execute(last_query)
-    conn.commit()
-
-        #close the connection
-cursor.close()
-conn.close()
+if __name__ == "__main__":
+    main()
